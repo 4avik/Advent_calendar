18ae8e5ca9b385abf8729bdb793025489158c228
Aavik <91154227+4avik@users.noreply.github.com>
2021-12-06 14:15:28 +0200

    Add files via upload

---
+++ Day 2/solution2.py
@@ -0,0 +1,17 @@
+f = open("example.txt", "r")
+
+lines = f.readlines()
+print(lines)
+horizontal = 0
+depth = 0
+
+for command in lines:
+    command = command.split()
+    print(command)
+    if command [0] == "forward":
+        horizontal += int(command[1]) 
+    elif command[0] == "down":
+        depth +int(command[1])
+    elif command[0] == "up":
+        depth -= int(command[1])
+    print(horizontal * depth)
